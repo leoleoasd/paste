19f3ea33eab3ea74799238ca3f1dbaca43c3ab69
Leo Lu <luyuxuanleo@gmail.com>
2019-10-28 21:13:21 +0800

    fixed bugs

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -32,6 +32,7 @@ class MainController extends Controller
             if($r->get("format") == 'on'){
                 exec("clang-format -style=\"{BasedOnStyle: llvm, IndentWidth: 4}\" ".storage_path("app/".$p->id.".cpp")." 2>&1",$code);
                 $code = join("\n", $code);
+                $code.="\n";
             }
             Storage::disk("local")->put($p->id.".cpp",$code);
             exec("docker run -v=".storage_path("app/".$p->id.".cpp").":/a.cpp  --rm clangbuiltlinux/ubuntu:latest /usr/lib/llvm-9/bin/clang /a.cpp -fsyntax-only -pedantic-errors -std=".$r->language." 2>&1",$syntax);
@@ -60,6 +61,7 @@ class MainController extends Controller
             if($r->get("format") == 'on'){
                 exec("clang-format -style=\"{BasedOnStyle: llvm, IndentWidth: 4}\" ".storage_path("app/".$p->id.".c")." 2>&1",$code);
                 $code = join("\n", $code);
+                $code.="\n";
             }
             Storage::disk("local")->put($p->id.".c",$code);
             exec("docker run -v=".storage_path("app/".$p->id.".c").":/a.c  --rm clangbuiltlinux/ubuntu:latest /usr/lib/llvm-9/bin/clang /a.c -fsyntax-only -pedantic-errors -std=".$r->language." 2>&1",$syntax);
@@ -75,10 +77,7 @@ class MainController extends Controller
         $p = new Paste();
         $p->language = $r->language != "-1" ? $r->language : "js";
         $code = base64_decode($r->code);
-        if($r->get("obfs") == 'on'){
-            $code = str_split($code);
-            $code = implode('‍‍‍',$code);
-        }
+        $code.="\n";
         $p->code= $code;
         $p->obfs = $r->get("obfs") == 'on';
         $p->syntax = "";
